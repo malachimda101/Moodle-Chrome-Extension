99de7a5e464b3f688479adf1077ec7ed5ed51fda
Malachi Ashley <malachimda101@gmail.com>
2021-04-19 11:09:51 -0400

    Update content.js

--- content.js
+++ content.js
@@ -5,7 +5,6 @@ for(i = 0; i < activity.length - 1; i++){
     var img = activity[i].getElementsByTagName("img")[0].src;
     if(img == "https://wesmoodle.wesleyan.edu/theme/image.php/boost/assign/1616582402/icon"){
         var cln = activity[i].cloneNode(true);
-        console.log(cln);
         var myDiv = document.createElement("div");
         myDiv.id = "assignment";
         myDiv.innerHTML = cln.innerHTML;
