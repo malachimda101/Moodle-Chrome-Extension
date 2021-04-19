1a5a66b1f57b62a19cd5c8d6867e6ff138a9dbc4
Malachi Ashley <malachimda101@gmail.com>
2021-04-19 10:54:27 -0400

    Moodle-Chrome-Extension
    Clones and moves all of the moodle assignments for a class moodle page to near the top of the page.

---
+++ content.js
@@ -0,0 +1,15 @@
+var activity = document.getElementsByClassName("activityinstance");
+
+var i;
+for(i = 0; i < activity.length - 1; i++){
+    var img = activity[i].getElementsByTagName("img")[0].src;
+    if(img == "https://wesmoodle.wesleyan.edu/theme/image.php/boost/assign/1616582402/icon"){
+        var cln = activity[i].cloneNode(true);
+        console.log(cln);
+        var myDiv = document.createElement("div");
+        myDiv.id = "assignment";
+        myDiv.innerHTML = cln.innerHTML;
+        document.getElementsByClassName("no-overflow")[0].appendChild(myDiv);
+    }
+}
+
